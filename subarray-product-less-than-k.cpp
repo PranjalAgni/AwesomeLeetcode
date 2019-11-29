5a3f53f454ce7cf7f438c2586f9302bd546e86c9
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2019-11-29 10:24:51 +0530

    Solved subarray-product-lesst-than-k :tada:

--- subarray-product-less-than-k.cpp
+++ subarray-product-less-than-k.cpp
@@ -1,5 +1,22 @@
 class Solution {
    public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k == 0) return 0;
+        int start = 0;
+        int end = 0;
+        int pro = 1;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            pro *= nums[i];
+            end = i;
+            if (pro >= k) {
+                while (pro >= k && start <= end) {
+                    pro /= nums[start];
+                    start += 1;
+                }
+            }
+            ans += end - start + 1;
+        }
+        return ans;
     }
 };
